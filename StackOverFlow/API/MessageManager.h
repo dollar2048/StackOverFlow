7ba91639bd73d8894e0337909edc6791983f757f
Sergei Prikhodko <dollar2048@gmail.com>
2015-11-25 22:52:57 +0300

    create MessageManager

---
+++ StackOverFlow/API/MessageManager.h
@@ -0,0 +1,13 @@
+//
+//  MessageManager.h
+//  StackOverFlow
+//
+//  Created by Sergei Prikhodko on 25.11.15.
+//  Copyright © 2015 Sergei Prikhodko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MessageManager : NSObject
+
+@end
---
+++ StackOverFlow/API/MessageManager.m
@@ -0,0 +1,18 @@
+//
+//  MessageManager.m
+//  StackOverFlow
+//
+//  Created by Sergei Prikhodko on 25.11.15.
+//  Copyright © 2015 Sergei Prikhodko. All rights reserved.
+//
+
+#import "MessageManager.h"
+
+static NSString *const kURLDomen = @"https://api.stackexchange.com/docs/";
+
+@interface MessageManager ()
+@end
+
+@implementation MessageManager
+
+@end
